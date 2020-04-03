e5e3911da3ae503f446f70e7b3645c87085904f4
Marcus Johnsen <cph-mj734@cphbusiness.dk>
2020-04-03 15:42:19 +0200

    tuesday and monday

---
+++ Monday/react-router-everything-you-need-to-know/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link,
+  useRouteMatch,
+  useParams,
+  useHistory
+} from "react-router-dom";
+import "./App.css";
+
+function App({ bookFacade }) {
+  return <div className="App"></div>;
+}
+
+export default App;
--- Monday/react-router-getting-started/src/App.js
+++ Monday/react-router-getting-started/src/App.js
@@ -1,12 +1,73 @@
 import React from "react";
-import "./App.css";
+import { HashRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import "./style2.css";
 
-function App() {
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <Router>
+      <div>
+        <ul className="header">
+          <li>
+            <NavLink exact activeClassName="selected" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact activeClassName="selected" to="/exercise1">
+              exercise1
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact activeClassName="selected" to="/exercise2">
+              exercise2
+            </NavLink>
+          </li>
+        </ul>
+
+        <hr />
+
+        {}
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
